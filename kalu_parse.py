f0e1d9cc3665ca3383d57653e6a9ae73edeae0ba
tdhris <tdhristova@yahoo.com>
2014-07-23 10:46:51 +0300

    refactoring a bit

--- kalu_parse.py
+++ kalu_parse.py
@@ -4,6 +4,29 @@ import os
 import inspect
 
 
+class Command:
+    SHORT_PREFIX = '-'
+    FULL_PREFIX = '--'
+
+    def __init__(self, short, full, help_message, default):
+        self._short = short
+        self._full = full
+        self.help_message = help_message
+        self._default = default
+
+    @property
+    def short(self):
+        return self.SHORT_PREFIX + self._short
+
+    @property
+    def full(self):
+        return self.FULL_PREFIX + self._full
+
+    @property
+    def default(self):
+        return self._default
+
+
 class KaluParser(CommandLineApp):
     _VERSION = "1.0"
 
@@ -18,13 +41,26 @@ class KaluParser(CommandLineApp):
 
     def setup(self):
         CommandLineApp.setup(self)
-        self.add_parameters()
+        commands = self.load_commands()
+        self.add_commands(commands)
 
-    def add_parameters(self):
-        self.add_param("-v", "--version",
-                       help="See the current version",
-                       default=False,
+    def add_commands(self, commands):
+        for command in commands:
+            self.add_command(command)
+
+    def add_command(self, command):
+        self.add_param(command.short, command.full,
+                       help=command.help_message,
+                       default=command.default,
                        action="count")
+
+    def load_commands(self):
+        commands = []
+        version_command = Command("v", "version",
+                                  "print the current version number and exit",
+                                  False)
+        commands.append(version_command)
+        return commands
 
     def main(self):
         if self.params.version:
