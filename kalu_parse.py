8c1d6e419ec5dc594202dc08ed3bb2a17c3d5db1
tdhris <tdhristova@yahoo.com>
2014-07-23 13:47:08 +0300

    refactoring

--- kalu_parse.py
+++ kalu_parse.py
@@ -4,50 +4,8 @@ import os
 import inspect
 
 
-class Command:
-    SHORT_PREFIX = '-'
-    FULL_PREFIX = '--'
-
-    def __init__(self, short, full, help_message, default, action, function):
-        self._short = short
-        self._full = full
-        self.help_message = help_message
-        self._default = default
-        self._action = action
-        self._function = function
-        self._name = full
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    @property
-    def short(self):
-        return self.SHORT_PREFIX + self._short
-
-    @property
-    def full(self):
-        return self.FULL_PREFIX + self._full
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def action(self):
-        return self._action
-
-    @property
-    def function(self):
-        return self._function
-
-    @property
-    def default(self):
-        return self._default
-
-
 class KaluParser(CommandLineApp):
     _VERSION = "1.0"
-    _COMMANDS = []
 
     @classmethod
     def get_version(cls):
@@ -60,32 +18,17 @@ class KaluParser(CommandLineApp):
 
     def setup(self):
         CommandLineApp.setup(self)
-        self.load_commands()
         self.add_commands()
 
     def add_commands(self):
-        for command in self._COMMANDS:
-            self.add_command(command)
-
-    def add_command(self, command):
-        self.add_param(command.short, command.full,
-                       help=command.help_message,
-                       default=command.default,
-                       action=command.action)
-        if command not in self._COMMANDS:
-            self._COMMANDS.append(command)
-
-    def load_commands(self):
-        self._COMMANDS = []
-        version_command = Command("v", "version",
-                                  "print the current version number and exit",
-                                  False, "store_true", self.print_version)
-        self._COMMANDS.append(version_command)
+        self.add_param("-v", "--version",
+                       help="print the current version number and exit",
+                       default=False,
+                       action="store_true")
 
     def main(self):
-        for command in self._COMMANDS:
-            if hasattr(self.params, command.name):
-                command.function()
+        if self.params.version:
+            self.print_version()
 
     def print_version(self):
         print(self.get_version())
--- tests.py
+++ tests.py
@@ -1,32 +1,11 @@
 import unittest
 from cli.test import AppTest
 from subprocess import check_output
-from kalu_parse import KaluParser, Command
+from kalu_parse import KaluParser
 import locale
 import os
 
 encoding = locale.getdefaultlocale()[1]
-
-
-class TestCommand(unittest.TestCase):
-    def setUp(self):
-        def foo():
-            pass
-
-        self.command = Command("l", "list", "lists dir", False, "store_true", foo)
-
-    def test_command_short_form_is_correct(self):
-        self.assertEquals('-l', self.command.short)
-
-    def test_command_full_form_is_correct(self):
-        self.assertEquals('--list', self.command.full)
-
-    def test_help_message_and_default_are_correct(self):
-        self.assertEquals('lists dir', self.command.help_message)
-        self.assertFalse(self.command.default)
-        new_help = "Blah"
-        self.command.help_message = new_help
-        self.assertEquals(new_help, self.command.help_message)
 
 
 class TestBasicKaluParserFunctions(AppTest):
@@ -41,14 +20,6 @@ class TestBasicKaluParserFunctions(AppTest):
 
     def test_get_path_returns_correct_path(self):
         self.assertIn('kalu_parse.py', os.listdir())
-
-    def test_a_command_can_be_added(self):
-        def blah():
-            print("blah")
-
-        command = Command("b", "blah", "prints blah", False, "store_true", blah)
-        self.app_cls.add_command(command)
-        self.assertIn(command, self.app_cls._COMMANDS)
 
 
 if __name__ == '__main__':
